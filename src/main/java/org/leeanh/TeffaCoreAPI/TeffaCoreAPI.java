088a32c63eb121e2046473318e14bac7c6434587
leeanh762k10 <leeanh762k10@gmail.com>
2026-07-01 11:53:36 +0700

    add header

--- src/main/java/org/leeanh/TeffaCoreAPI/TeffaCoreAPI.java
+++ src/main/java/org/leeanh/TeffaCoreAPI/TeffaCoreAPI.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2026 Nguyễn Lê Anh.
+ *
+ * This file is part of TeffaCoreAPI.
+ * All rights reserved.
+ */
 package org.leeanh.TeffaCoreAPI;
 
 import net.luckperms.api.LuckPerms;
